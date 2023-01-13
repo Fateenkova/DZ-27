215cdf660b2bddeec7779fbe746d1a2770a7c290
Helen <zybina.e@mail.ru>
2023-01-13 12:28:24 +0300

    ДЗ c# задача 27

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum=0;
+while(num>0)
+{
+    sum=sum+num%10;
+    num=num/10;
+}
+Console.WriteLine(sum);
+
